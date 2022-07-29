b289e83fe40b411e6bd49ea7157cbf2a292358ae
Ornella delfino <ornella.delfino@hotmail.com>
2022-07-29 01:02:53 +0100

    foro terminado. comence articulo

--- routes/articulo.js
+++ routes/articulo.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
+var articulosModel = require('../models/articulosModel')
 /* GET articulo page. */
-router.get('/', function(req, res, next) {
-  res.render('articulo');
+
+router.get('/:id', async function(req, res, next) {
+  var id = req.params.id;
+  var articulo = await articulosModel.getArticuloByID(id);
+  res.render('articulo', {
+    articulo,
+    isForo: true
+  });
 });
 
 module.exports = router;
--- routes/foro.js
+++ routes/foro.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var articulosModel = require('../models/articulosModel'); 
 
-/* GET foro page. */
-router.get('/', function(req, res, next) {
-  res.render('foro', {
-    isForo:true
-  });
+router.get('/', async function(req, res, next) {
+  var articulos;
+  if(req.query.q === undefined){
+      articulos = await articulosModel.getArticulos();
+  }else{
+      articulos = await articulosModel.buscarArticulos(req.query.q);
+  }
+   res.render('foro', {
+       isForo: true,
+       articulos,
+       is_search: req.query.q !== undefined,
+       q:req.query.q 
+   });
+   
 });
-
 module.exports = router;
